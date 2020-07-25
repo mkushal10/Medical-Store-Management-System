8bd52f002dff3ecf73eafa7adad314881a3f0dd4
Kushal Neupane <mkushal10@gmail.com>
2020-07-25 09:27:39 +0545

    All Serializers and Medicine Viewset

--- Medical/serializers.py
+++ Medical/serializers.py
@@ -1,20 +1,94 @@
 #importing serializers
 from rest_framework import serializers
-from Medical.models import Company, CompanyBank
+
+from Medical.models import Company, CompanyBank, Medicine, MedicalDetails, Employee, Customer, Bill, \
+    CustomerRequest, CompanyAccount, EmployeeBank
 
 
 class CompanySerliazer(serializers.ModelSerializer):
     class Meta:
-        model = Company
-        #fields = ["name","license_no","address","contact_no"]
-        fields = "__all__"
+        model=Company
+        # fields = ["name","license_no","address","contact_no"]
+        fields="__all__"
 
 class CompanyBankSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CompanyBank
-        fields = "__all__"
+        model=CompanyBank
+        fields="__all__"
+
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['company']=CompanySerliazer(instance.company_id).data
+        return response
+
+
+class MedicineSerliazer(serializers.ModelSerializer):
+    class Meta:
+        model=Medicine
+        fields="__all__"
+
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['company']=CompanySerliazer(instance.company_id).data
+        return response
+
+
+
+class MedicalDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MedicalDetails
+        fields="__all__"
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['medicine'] = MedicineSerliazer(instance.medicine_id).data
+        return response
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Employee
+        fields="__all__"
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Customer
+        fields="__all__"
+
+class BillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Bill
+        fields="__all__"
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['customer'] = CustomerSerializer(instance.customer_id).data
+        return response
+
+class CustomerRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CustomerRequest
+        fields="__all__"
+
+
+class CompanyAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CompanyAccount
+        fields="__all__"
 
     def to_representation(self, instance):
         response = super().to_representation(instance)
         response['company'] = CompanySerliazer(instance.company_id).data
+        return response
+
+
+class EmployeeBankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=EmployeeBank
+        fields="__all__"
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['employee'] = EmployeeSerializer(instance.employee_id).data
         return response
--- Medical/views.py
+++ Medical/views.py
@@ -6,12 +6,11 @@ from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from Medical.models import Company, CompanyBank, Medicine
+from Medical.serializers import CompanySerliazer, CompanyBankSerializer, MedicineSerliazer
 
-from Medical.models import Company, CompanyBank
-from Medical.serializers import CompanySerliazer, CompanyBankSerializer
 
-
-#OLD Viewset
+# OLD Viewset
 # class CompanyViewSet(viewsets.ModelViewSet):
 #     queryset = Company.objects.all()
 #     serializer_class = CompanySerliazer
@@ -21,32 +20,32 @@ class CompanyViewSet(viewsets.ViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def list(self,request):
-        company=Company.objects.all()
-        serializer=CompanySerliazer(company,many=True,context={"request":request})
-        response_dict={"error":False,"message":"All Company List Data","data":serializer.data}
+    def list(self, request):
+        company = Company.objects.all()
+        serializer = CompanySerliazer(company, many=True, context={"request": request})
+        response_dict = {"error": False, "message": "All Company List Data", "data": serializer.data}
         return Response(response_dict)
 
-    def create(self,request):
+    def create(self, request):
         try:
-            serializer=CompanySerliazer(data=request.data,context={"request":request})
+            serializer = CompanySerliazer(data=request.data, context={"request": request})
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            dict_response={"error":False,"message":"Company Data Save Successfully"}
+            dict_response = {"error": False, "message": "Company Data Save Successfully"}
         except:
-            dict_response={"error":True,"message":"Error During Saving Company Data"}
+            dict_response = {"error": True, "message": "Error During Saving Company Data"}
         return Response(dict_response)
 
-    def update(self,request,pk=None):
+    def update(self, request, pk=None):
         try:
-            queryset=Company.objects.all()
-            company=get_object_or_404(queryset,pk=pk)
-            serializer=CompanySerliazer(company,data=request.data,context={"request":request})
+            queryset = Company.objects.all()
+            company = get_object_or_404(queryset, pk=pk)
+            serializer = CompanySerliazer(company, data=request.data, context={"request": request})
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            dict_response={"error":False,"message":"Successfully Updated Company Data"}
+            dict_response = {"error": False, "message": "Successfully Updated Company Data"}
         except:
-            dict_response={"error":True,"message":"Error During Updating Company Data"}
+            dict_response = {"error": True, "message": "Error During Updating Company Data"}
 
         return Response(dict_response)
 
@@ -54,44 +53,81 @@ class CompanyViewSet(viewsets.ViewSet):
 class CompanyBankViewset(viewsets.ViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-    def create(self,request):
+
+    def create(self, request):
         try:
-            serializer=CompanyBankSerializer(data=request.data,context={"request":request})
+            serializer = CompanyBankSerializer(data=request.data, context={"request": request})
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            dict_response={"error":False,"message":"Company Bank Data Save Successfully"}
+            dict_response = {"error": False, "message": "Company Bank Data Save Successfully"}
         except:
-            dict_response={"error":True,"message":"Error During Saving Company Bank Data"}
+            dict_response = {"error": True, "message": "Error During Saving Company Bank Data"}
         return Response(dict_response)
 
-    def list(self,request):
-        companybank=CompanyBank.objects.all()
-        serializer=CompanyBankSerializer(companybank,many=True,context={"request":request})
-        response_dict={"error":False,"message":"All Company Bank List Data","data":serializer.data}
+    def list(self, request):
+        companybank = CompanyBank.objects.all()
+        serializer = CompanyBankSerializer(companybank, many=True, context={"request": request})
+        response_dict = {"error": False, "message": "All Company Bank List Data", "data": serializer.data}
         return Response(response_dict)
 
-    def retrieve(self,request,pk=None):
-        queryset=CompanyBank.objects.all()
-        companybank=get_object_or_404(queryset,pk=pk)
-        serializer=CompanyBankSerializer(companybank,context={"request":request})
-        return Response({"error":False,"message":"Single Data Fetch","data":serializer.data})
+    def retrieve(self, request, pk=None):
+        queryset = CompanyBank.objects.all()
+        companybank = get_object_or_404(queryset, pk=pk)
+        serializer = CompanyBankSerializer(companybank, context={"request": request})
+        return Response({"error": False, "message": "Single Data Fetch", "data": serializer.data})
 
-    def update(self,request,pk=None):
-        queryset=CompanyBank.objects.all()
-        companybank=get_object_or_404(queryset,pk=pk)
-        serializer=CompanyBankSerializer(companybank,data=request.data,context={"request":request})
+    def update(self, request, pk=None):
+        queryset = CompanyBank.objects.all()
+        companybank = get_object_or_404(queryset, pk=pk)
+        serializer = CompanyBankSerializer(companybank, data=request.data, context={"request": request})
         serializer.is_valid()
         serializer.save()
-        return Response({"error":False,"message":"Data Has Been Updated"})
+        return Response({"error": False, "message": "Data Has Been Updated"})
 
 
 class CompanyNameViewSet(generics.ListAPIView):
     serializer_class = CompanySerliazer
+
     def get_queryset(self):
-        name=self.kwargs["name"]
+        name = self.kwargs["name"]
         return Company.objects.filter(name=name)
 
 
-company_list=CompanyViewSet.as_view({"get":"list"})
-company_creat=CompanyViewSet.as_view({"post":"create"})
-company_update=CompanyViewSet.as_view({"put":"update"})
+class MedicineViewSet(viewsets.ViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request):
+        try:
+            serializer = MedicineSerliazer(data=request.data, context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            dict_response = {"error": False, "message": "Medicine Data Save Successfully"}
+        except:
+            dict_response = {"error": True, "message": "Error During Saving Medicine Data"}
+        return Response(dict_response)
+
+    def list(self, request):
+        medicine = Medicine.objects.all()
+        serializer = MedicineSerliazer(medicine, many=True, context={"request": request})
+        response_dict = {"error": False, "message": "All Medicine List Data", "data": serializer.data}
+        return Response(response_dict)
+
+    def retrieve(self, request, pk=None):
+        queryset = Medicine.objects.all()
+        medicine = get_object_or_404(queryset, pk=pk)
+        serializer = MedicineSerliazer(medicine, context={"request": request})
+        return Response({"error": False, "message": "Single Data Fetch", "data": serializer.data})
+
+    def update(self, request, pk=None):
+        queryset = Medicine.objects.all()
+        medicine = get_object_or_404(queryset, pk=pk)
+        serializer = MedicineSerliazer(medicine, data=request.data, context={"request": request})
+        serializer.is_valid()
+        serializer.save()
+        return Response({"error": False, "message": "Data Has Been Updated"})
+
+
+company_list = CompanyViewSet.as_view({"get": "list"})
+company_creat = CompanyViewSet.as_view({"post": "create"})
+company_update = CompanyViewSet.as_view({"put": "update"})
